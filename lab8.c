e4997cdf785df5e4e253aaa43701140e7b79e4a3
Xylobyte <dxgriego@gmail.com>
2020-11-08 17:17:39 -0700

    Completed lab8.c source

--- lab8.c
+++ lab8.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define BUFFER 20
 
 
@@ -31,7 +32,7 @@ int main() {
         printf("2) Delete a number\n");
         printf("3) Print all numbers\n");
         printf("4) Tell how many items are in the list\n");
-        printf("5) Find if a number in the list\n");
+        printf("5) Find a number in the list\n");
         printf("6) Quit\n");
         printf(">> ");
         fgets(in, BUFFER, stdin);
@@ -60,20 +61,36 @@ int main() {
                         printf(">> ");
                         fgets(in, BUFFER, stdin);
                         sscanf(in, "%d", &intIn);
+                        head = insert_middle(head, create_node(doubleIn), intIn);
                         break;
                 }
                 break;
             case 2:
-                
+                printf("Enter a value to delete from the list\n");
+                printf(">> ");
+                fgets(in, BUFFER, stdin);
+                sscanf(in, "%lf", &doubleIn);
+                head = delete_node(head, doubleIn);
                 break;
             case 3:
                 print_list(head);
                 break;
             case 4:
-
+                printf("\nThere are %d items in the list.\n", count_nodes(head));
                 break;
             case 5:
-
+                printf("Enter a value to find in the list\n");
+                printf(">> ");
+                fgets(in, BUFFER, stdin);
+                sscanf(in, "%lf", &doubleIn);
+                struct node_t *n = find_node(head, doubleIn);
+                if(n != NULL) {
+                    printf("\nThe value was found in the list\n");
+                    printf("%-15s %-15s %-15s\n", "Node", "Data", "Next");
+                    print_node(n);
+                } else {
+                    printf("\nThe value was not found in the list\n");
+                }
                 break;
             case 6:
                 delete_list(head);
@@ -92,10 +109,11 @@ struct node_t *create_node(double n) {
 }
 
 void print_node(struct node_t *node) {
-    printf("Node: %p   Data: %lf   Next: %p\n", node, node->data, node->next);
+    printf("%-15p %-15lf %-15p\n", node, node->data, node->next);
 }
 
 void print_list(struct node_t *head) {
+    printf("\n%-15s %-15s %-15s\n", "Node", "Data", "Next");
     while (head != NULL) 
     { 
         print_node(head);
@@ -122,21 +140,105 @@ struct node_t * insert_tail(struct node_t *head, struct node_t *node) {
 }
 
 struct node_t *insert_middle(struct node_t *head, struct node_t *node, int pos) {
-    return head;
+    if ((head == NULL || pos == 1) && pos > 0){
+        return insert_head(head, node);
+    }else if (pos > 1) {
+        struct node_t *h = head;
+        int i = 2;
+        while (head->next != NULL && i != pos) 
+        {
+            head = head->next; 
+            i++;
+        }
+        node->next = head->next;
+        head->next = node;
+        return h;
+    } else {
+        free(node);
+        printf("\nThat is not a valid position; Number was not recorded.\n");
+        return head;
+    }
 }
 
 int count_nodes(struct node_t *head) {
-    return 0;
+    if(head == NULL){
+        return 0;
+    }
+    int i = 1;
+    while (head->next != NULL) 
+        {
+            head = head->next; 
+            i++;
+        }
+    return i;
 }
 
 struct node_t *find_node(struct node_t *head, double n) {
-    return head;
+    if(head == NULL){
+        printf("\nThere are no numbers in the list\n");
+        return NULL;
+    }
+    int found = 0;
+    while (head != NULL) 
+    {
+        if(isgreaterequal(head->data, n) && islessequal(head->data, n)){
+            found = 1;
+            break;
+        }
+        head = head->next;
+    }
+    if(found) {
+        return head;
+    } else {
+        return NULL;
+    }
 }
 
 struct node_t *delete_node(struct node_t *head, double n) {
-    return head;
+    if(head == NULL){
+        printf("\nThere are no numbers in the list\n");
+        return head;
+    }
+    struct node_t *h = head;
+    struct node_t *prev;
+    int i = 1;
+    int found = 0;
+    while (head != NULL) 
+    {
+        if(isgreaterequal(head->data, n) && islessequal(head->data, n)){
+            found = 1;
+            break;
+        }
+        prev = head;
+        head = head->next; 
+        i++;
+    }
+    if(found) {
+        if(i == 1){
+            h = head->next;
+            free(head);
+        } else if(head->next == NULL) {
+            prev->next = NULL;
+            free(head);
+        } else {
+            prev->next = head->next;
+            free(head);
+        }
+    } else {
+        printf("\nThe number was not found in the list; nothing was deleted\n");
+    }
+    return h;
 }
 
 void delete_list(struct node_t *head) {
-
+    if(head == NULL){
+        return;
+    }
+    struct node_t *next = head;
+    while (head != NULL) 
+    {
+        next = head->next;
+        free(head);
+        head = next;
+    }
 }
